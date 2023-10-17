e23a5621463495b4b34078453fe38c7236d464e4
Hannah Aubry <hannah.aubry@gmail.com>
2023-10-17 16:01:27 -0400

    Add files to main

---
+++ src/index.js
@@ -0,0 +1,37 @@
+/// <reference types="@fastly/js-compute" />
+import { CacheOverride } from "fastly:cache-override";
+import { SecretStore } from "fastly:secret-store";
+
+const backendName = "rt";
+const serviceId = "7aoz1CBbiHDltvDsuAmhnM";
+
+addEventListener("fetch", (event) => event.respondWith(handleRequest(event)));
+
+async function handleRequest(event) {
+  // Get the request from the client.
+  const req = event.request;
+  
+  // Parse the URL from client request.
+  const url = new URL(req.url);
+  const datetime = url.searchParams.get("ts");
+  const newUrl = `https://rt.fastly.com/v1/channel/${serviceId}/ts/${datetime}`;
+  console.log(newUrl);
+  
+  // Get api token from secret store
+  const secrets = new SecretStore('tokens')
+  const key = await secrets.get('bradbenvenuti');
+
+  // Create a cache override.
+  let cacheOverride = new CacheOverride("override", { ttl: 60 });
+  
+  return fetch(newUrl, {
+    backend: backendName,
+    cacheOverride,
+    headers: {
+      "Fastly-Key": key.plaintext()
+    }
+  });
+}
+
+
+
